3750561766ee9fcb29dc6edad5fd2e01571d8371
pravink123 <pravinkasabe.pk145@gmail.com>
2022-12-24 18:36:17 +0530

    first comment : code

---
+++ src/com/velocity/demo/Student.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+ public static void main(String[] args) {
+	
+	 List<String> list=new ArrayList<String>();
+	 list.add("Pravin");
+	 list.add("Suraj");
+	 list.add("Datta");
+	 
+	 System.out.println(list);
+}
+}
---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 List<String> list=new ArrayList<String>();
+		 list.add("Pravin");
+		 list.add("Suraj");
+		 list.add("Datta");
+		 
+		 System.out.println(list);
+	}
+}
