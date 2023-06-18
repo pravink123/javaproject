378429ce2772e5439fc8de8f14c3682cbf2dc3b5
Pravin <pravinkasabe.pk145@gmail.com>
2023-06-18 13:37:39 +0530

    This is latest

---
+++ src/com/velocity/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.velocity.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello How are you Latest/////");
+	}
+
+}
